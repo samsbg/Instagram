7a8d14c083e45069c027d2bbc12fdd1b8d03b469
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-06 16:21:48 -0500

    Request posts from the online database

--- app/src/main/java/com/codepath/instagram/activities/MainActivity.java
+++ app/src/main/java/com/codepath/instagram/activities/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.codepath.instagram.R;
+import com.codepath.instagram.models.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +24,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        queryPosts();
+    }
 
+    private void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e("MainActivity", "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i("MainActivity", "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 
     @Override
