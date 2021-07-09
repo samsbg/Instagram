4bbc16b0cbb922e717bb62c5f71da8ce1337ebc5
Samantha Bautista Gauna <sambautista@fb.com>
2021-07-09 17:30:56 -0500

    Profile activity

---
+++ app/src/main/java/com/codepath/instagram/activities/ProfileActivity.java
@@ -0,0 +1,82 @@
+package com.codepath.instagram.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.codepath.instagram.R;
+import com.codepath.instagram.adapters.ProfilePostAdapter;
+import com.codepath.instagram.models.Post;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.FindCallback;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.reverse;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    RecyclerView rvUserFeed;
+    List<Post> posts;
+    ProfilePostAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.optPost:
+                        Intent i = new Intent(ProfileActivity.this, CreatePostActivity.class);
+                        startActivity(i);
+                        return true;
+                    case R.id.optFeedView:
+                        Intent j = new Intent(ProfileActivity.this, FeedActivity.class);
+                        startActivity(j);
+                        return true;
+                    default: return true;
+                }
+            }
+        });
+
+        posts = new ArrayList<>();
+        adapter = new ProfilePostAdapter(this, posts);
+
+        rvUserFeed = findViewById(R.id.rvUserFeed);
+        rvUserFeed.setLayoutManager(new GridLayoutManager(this, 3));
+        rvUserFeed.setAdapter(adapter);
+        queryProfilePosts();
+    }
+
+    private void queryProfilePosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.whereEqualTo("user", ParseUser.getCurrentUser());
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> postsList, com.parse.ParseException e) {
+                if (e != null) {
+                    Log.e("FeedActivity", "Issue with getting posts", e);
+                    return;
+                }
+                reverse(postsList);
+                posts.addAll(postsList);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/instagram/adapters/ProfilePostAdapter.java
@@ -0,0 +1,96 @@
+package com.codepath.instagram.adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.instagram.R;
+import com.codepath.instagram.models.Post;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+
+import java.util.List;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
+
+public class ProfilePostAdapter extends RecyclerView.Adapter<ProfilePostAdapter.ViewHolder> {
+
+    Context context;
+    List<Post> posts;
+
+    public ProfilePostAdapter(Context context, List<Post> posts) {
+        super();
+        this.context = context;
+        this.posts = posts;
+    }
+
+    @Override
+    public ProfilePostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_profile_post, parent, false);
+        return new ProfilePostAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProfilePostAdapter.ViewHolder holder, int position) {
+        Post post = posts.get(position);
+        holder.bind(post);
+    }
+
+    @Override
+    public int getItemCount() {
+        return posts.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        posts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> list) {
+        posts.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView ivProfilePost;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivProfilePost = itemView.findViewById(R.id.ivProfilePost);
+        }
+
+        public void bind(final Post post) {
+            if (post.getImage() != null) {
+                post.getImage().getDataInBackground(new GetDataCallback() {
+                    @Override
+                    public void done(byte[] data, ParseException e) {
+                        if (e == null) {
+                            Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                            Glide.with(context).load(bmp).into(ivProfilePost);
+                            ivProfilePost.setVisibility(VISIBLE);
+                        } else {
+                            Log.e("PostAdapter", "Image not loading", e);
+                        }
+                    }
+                });
+            } else {
+                ivProfilePost.setVisibility(GONE);
+            }
+        }
+
+    }
+}
